7b94b1dc2c2b17e451cb1bcf04ec0acbf1e6b57b
zhanghuaqiang <zhanghuaqiang@bykernel.com>
2018-11-23 09:52:29 +0800

    add DEBUG_LOG to .env

--- global.js
+++ global.js
@@ -1,6 +1,13 @@
 // Inject node globals into React Native global scope.
 import { URL, URLSearchParams } from "whatwg-url";
 // Inject node globals into React Native global scope.
+import Config from "react-native-config"
+
+// disable console.log in production mode
+if(Config.DEBUG_LOG == '0'){
+  console.log = ()=>{};
+}
+
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 global.btoa = require('btoa');
