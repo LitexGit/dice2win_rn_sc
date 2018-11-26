0d6a965f9a79461c57438ebcf84f1ba6f7545628
zhanghuaqiang <zhanghuaqiang@bykernel.com>
2018-11-26 19:10:37 +0800

    add web3 status checking

--- App/Lib/Provider/provider.js
+++ App/Lib/Provider/provider.js
@@ -10,10 +10,10 @@ async function checkWeb3Status() {
     console.tron.log('web3 is ok', result);
   }catch(err){
     web3.setProvider(new Web3.providers.WebsocketProvider(ethWSUrl));
-    // if (global.scclient != null) {
-    //   Toast.show('init web3 here' + err);
-    //   global.scclient.initWeb3(web3);
-    // }
+    if (global.scclient != null) {
+      Toast.show('init web3 here' + err);
+      global.scclient.initWeb3(web3);
+    }
   }
 }
 
